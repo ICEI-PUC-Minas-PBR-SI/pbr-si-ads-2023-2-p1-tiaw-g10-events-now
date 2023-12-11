e38df98c12d43285aa940f3bcf84e43b9367e7ca
nathancarvalhocota <64663542+nathancarvalhocota@users.noreply.github.com>
2023-12-11 12:04:33 -0300

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const url = 'https://jsonserver-cachoeiras--dantonlucassaga.repl.co/Cachoeiras';
-let Cachoeiras = [];
-fetch(url)
-    .then(function (response) { return response.json() })
-    .then(function (dados) {
-        Cachoeiras = dados;
-        console.log('Dados de Cachoeiras Carregados!');
-    });
